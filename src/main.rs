615134bd021e29ad0aa9705c05f0bf331ae6f106
Sabin Maharjan <smaharjan02@student.ysu.edu>
2023-04-06 00:56:33 -0400

    Sample, parser and hashtable

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[allow(dead_code)]
 
 mod parser;
-use std::{fs::File, io::{BufReader, BufRead}};
+use regex::Regex;
+use std::collections::HashMap;
+use std::{fs::{File, self}, io::{BufReader, BufRead}};
 use rusqlite::{Connection, Result};
 use rand::seq::IteratorRandom;
 
-#[derive(Clone,Debug)]#[allow(dead_code)]
-
+#[derive(Clone,Debug)]
 struct Lineitem {
     l_orderkey: i32,
     l_partkey: i32,
@@ -79,42 +80,116 @@ fn create_sample(db_file: &str, sample_fraction: f64) -> Result<Vec<Lineitem>, B
     Ok(sample)
 }
 
-// fn query_result(sample:&Vec<Lineitem>, query: &str) -> f64 {
-//     let mut result = 0.0;
-//     for row in sample {
-//         if query == "l_quantity" {
-//             result += row.l_quantity;
-//         }
-//     }
-//     result
-// }
 
-// fn is_column_in_file(column_name: &str, file_path: &str) -> Option<String> {
-//     let contents = fs::read_to_string(file_path).expect("Failed to read file");
+fn parse_column_names(sql: &str) -> Vec<String> {
+    let column_re = Regex::new(r"(?i)(\w+)\s+[\w\(\),]+(\s+NOT NULL)?").unwrap();
+    column_re
+        .captures_iter(sql)
+        .map(|cap| cap[1].to_string())
+        .filter(|name| name.to_uppercase() != "CREATE" && name.to_uppercase() != "LINEITEM")
+        .collect()
+}
 
-//     contents.lines().enumerate().find_map(|(_, line)| {
-//         if line.contains(column_name) {
-//             Some(column_name.to_string())
-//         } else {
-//             None
-//         }
-//     })
-// }
+//Creating a hashmap using column names as keys and lineitem struct values as values
+fn lineitem_to_hashmap(lineitems: &[Lineitem], column_names: &Vec<String>) -> Vec<HashMap<String, String>> {
+    let mut hashmaps = Vec::new();
+    
+    for lineitem in lineitems {
+        let mut hashmap = HashMap::new();
+        for column_name in column_names {
+            match column_name.to_lowercase().as_str() {
+                "l_orderkey" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_orderkey.to_string());
+                }
+                "l_partkey" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_partkey.to_string());
+                }
+                "l_suppkey" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_suppkey.to_string());
+                }
+                "l_linenumber" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_linenumber.to_string());
+                }
+                "l_quantity" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_quantity.to_string());
+                }
+                "l_extendedprice" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_extendedprice.to_string());
+                }
+                "l_discount" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_discount.to_string());
+                }
+                "l_tax" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_tax.to_string());
+                }
+                "l_returnflag" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_returnflag.to_string());
+                }
+                "l_linestatus" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_linestatus.to_string());
+                }
+                "l_shipdate" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_shipdate.to_string());
+                }
+                "l_commitdate" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_commitdate.to_string());
+                }
+                "l_receiptdate" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_receiptdate.to_string());
+                }
+                "l_shipinstruct" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_shipinstruct.to_string());
+                }
+                "l_shipmode" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_shipmode.to_string());
+                }
+                "l_comment" => {
+                    hashmap.insert(column_name.clone(), lineitem.l_comment.to_string());
+                }
+                _ => {
+                    println!("Not found");
+                }            
+            };
+        }
+        hashmaps.push(hashmap);
+    }
+    hashmaps
+}
 
 
 fn main(){
     let sample = create_sample("table100m.db", 0.1).unwrap();
 
-    println!("Sample size: {:#?}", sample.len());
+    //parsing the create table schema to get the column names
+    let create_table_column = fs::read_to_string("create_table.txt")
+        .expect("Unable to read file");
+    let column_names = parse_column_names(&create_table_column);
 
-    let file = File::open("query.sql"). expect("Unable to open file");
-    let reader = BufReader::new(file);
 
-    for (index,line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        let (_, statement) = parser::parse_select_statement(&line).unwrap();
-        println!("Line number {} \n {:#?}", index+1, statement);
+    let hashed_table = lineitem_to_hashmap(&sample, &column_names);
+
+    println!("{:#?}", hashed_table.len());
+    // for hashmap in hashed_table {
+    //     println!("{:#?}", hashmap.len());
+    // }
+
+    
+
+    
+    // println!("Sample size: {:#?}", sample.len());
+
+    // let file = File::open("query.txt"). expect("Unable to open file");
+    // let reader = BufReader::new(file);
+
+    // for (index,line) in reader.lines().enumerate() {
+    //     let line = line.unwrap();
+    //     let (_, statement) = parser::parse_select_statement(&line).unwrap();
+    //     println!("Line number {} \n {:#?}", index+1, statement );
+    // }
+
+    
 }
-}
+
+
 
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 
-
+// Used libraries
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while, tag_no_case},
@@ -9,6 +9,7 @@ use nom::{
     IResult,
 };
 
+//represents a parsed SQL statement
 #[derive(Debug, PartialEq)]
 pub struct SelectStatement {
     function: String,
@@ -23,15 +24,17 @@ pub struct SelectStatement {
 
 }
 
+//parses a SQL identifier
 fn identifier(input: &str) -> IResult<&str, &str> {
     take_while(|c: char| c.is_alphanumeric() || c == '_' || c.is_uppercase())(input)
 }
 
-
+//parsing an integer
 fn parse_integer(input: &str) -> IResult<&str, &str> {
     digit1(input)
 }
 
+//parsing a float (.5, 10.55)
 fn parse_float(input: &str) -> IResult<&str, &str> {
     recognize(tuple((
         opt(digit1),
@@ -39,7 +42,7 @@ fn parse_float(input: &str) -> IResult<&str, &str> {
         opt(tuple((digit1, opt(digit1)))),
     )))(input)
 }
-
+//combining the two functions above to parse a number
 fn parse_number(input: &str) -> IResult<&str, &str> {
     alt((
         parse_float,
@@ -48,8 +51,7 @@ fn parse_number(input: &str) -> IResult<&str, &str> {
     ))(input)
 }
 
-
-
+//parsing the select and retrieving the count function
 fn parse_select(input:&str) -> IResult<&str, Option<&str>> {
     let (input , _) = space0(input)?;
     let (input, _) = tag_no_case("select")(input)?;
@@ -62,6 +64,7 @@ fn parse_select(input:&str) -> IResult<&str, Option<&str>> {
 
 }
 
+//parsing from and retrieving the table name and alias
 fn parse_from(input: &str) -> IResult<&str, (&str, &str)> {
     let (input, _) = tag_no_case("from")(input)?;
     let (input, _) = space0(input)?;
@@ -74,6 +77,7 @@ fn parse_from(input: &str) -> IResult<&str, (&str, &str)> {
     Ok((input, (table,alias)))
 }
 
+//parsing where and retrieving the column name, comparator and value
 fn parse_where(input: &str) -> IResult<&str, (&str, &str, &str)> {
     let (input, _) = tag_no_case("where")(input)?;
     let (input, _) = space0(input)?;
@@ -90,6 +94,7 @@ fn parse_where(input: &str) -> IResult<&str, (&str, &str, &str)> {
     Ok((input, (where_cond_column, where_cond_comparator, where_cond_value)))
 }
 
+//parsing and and retrieving the column name, comparator and value
 fn parse_and(input: &str) -> IResult<&str, (&str, &str, &str)> {
     let (input, _) = space0(input)?;
     let (input, _) = tag_no_case("and")(input)?;
@@ -107,15 +112,11 @@ fn parse_and(input: &str) -> IResult<&str, (&str, &str, &str)> {
     Ok((input, (and_cond_column, and_cond_comparator, and_cond_value)))
 }
 
+//combining all the functions above to parse a select statement
 pub fn parse_select_statement (input: &str) -> IResult<&str, SelectStatement> {
     let (input , function) = parse_select(input).unwrap();
-
     let (input, (table, alias),)   = parse_from(input).unwrap();
-
-    
-
     let (input, (where_cond_column, where_cond_comparator, where_cond_value)) = parse_where(input).unwrap();
-
     let (input, (and_cond_column, and_cond_comparator, and_cond_value)) = parse_and(input).unwrap();
 
     let statement = SelectStatement {
